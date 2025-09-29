15ebadbb3f7f2b88e4594a777325247ac8f33f8a
Muaz Ahmad Khan <k250798@nu.edu.pk>
2025-09-29 10:09:14 +0500

    Add files via upload

---
+++ PIN Lab6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int const key=2025;
+	int pin;
+	printf("Enter your pin: ");
+	scanf("%d", &pin);
+	while (pin!=key){
+		printf("\nWRONG PIN!\nRe-Enter your pin: ");
+	    scanf("%d", &pin);
+	}
+	printf("PIN is Correct");
+}
